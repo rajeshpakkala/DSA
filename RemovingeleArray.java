b593a2d3e463457a848feee6c80ff19b99a156c7
rajeshpakkala <rajeshpakkala786@gmail.com>
2023-08-06 22:25:11 +0530

    Duplicate element removes in array

--- RemovingeleArray.java
+++ RemovingeleArray.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 public class RemovingeleArray{
   public static void main(String[]args){
     int []arr={3,4,5,55,6,6,777,888};
-    int remove=3;
+     System.out.println(Arrays.toString(arr));
+    int remove=2;
     for(int i=remove;i<arr.length-1;i++){
-        arr[i]=arr[i+1];
+      arr[i]=arr[i+1];
     }
     System.out.println(Arrays.toString(arr));
      
